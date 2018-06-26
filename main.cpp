bc9fb990dd4e8a3010401b793e48ae2989b92cc3
Amar <amarsgithub@outlook.com>
2018-06-26 13:38:44 -0400

    Get function works

--- main.cpp
+++ main.cpp
@@ -4,17 +4,23 @@ int main(void)
 {
     LinkedList myList;
 
+    // Spec 1 asked by google: add an element
     myList.add(5);
     myList.add(16);
     myList.add(17);
 
     myList.printList();
 
-    // LinkedList::Node *someNode = myList.get(2);
-    //
-    // cout << someNode->data << endl;
+    // Spec 2 asked by Google: access an element at an index
+    try {
+        LinkedList::Node *someNode = myList.get(7);
+        cout << someNode->data << endl;
+    } catch (int e)
+    {
+        cout << "Error in accessing element." << endl;
+    }
 
-    
+
 
 
     return 0;
